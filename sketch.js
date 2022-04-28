46c9074de5f28ffd10ad828004fa931446356d5f
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-28 08:53:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,20 +12,17 @@ var gameState = START;
 
 function preload(){
   jack_image=loadImage("images/jack2.png");
-platform1 = loadImage("images/platform1.png");
-platform2 = loadImage("images/platform2.png");
-platform3 = loadImage("images/platform3.png");
-platform4 = loadImage("images/platform4.png");
-platform5 = loadImage("images/platform5.png");
-platform6 = loadImage("images/platform6.png");
-bgImg = loadImage('images/bg.png');
+  platform1 = loadImage("images/platform1.png");
+  platform2 = loadImage("images/platform2.png");
+  platform3 = loadImage("images/platform3.png");
+  platform4 = loadImage("images/platform4.png");
+  platform5 = loadImage("images/platform5.png");
+  platform6 = loadImage("images/platform6.png");
+  bgImg = loadImage('images/bg.png');
 }
 
 function setup() {
   createCanvas(500, 500);
-  bg = createSprite(250,250,500,500);
-  bg.addImage(bgImg)
-  bg.scale =0.2;
 
 
   player = createSprite(250,300,20,50);
@@ -38,20 +35,20 @@ function setup() {
 }
 
 function draw() {
-  background(bg);
+  background(bgImg);
   if(gameState==START)
   {
-     if(keyDown("up"))
-  {
-  player.velocityY=-10;
-  gameState =PLAY;
-  }
+    if(keyDown("up"))
+    {
+      player.velocityY=-10;
+      gameState =PLAY;
+    }
 
  
-  fill(255);
-  textSize(16);
-  text("Press Space to make jack jump",130,100);
-  text("Press right and left arrow to make jack go right and left",50,80);
+    fill(255);
+    textSize(16);
+    text("Press Space to make jack jump",130,100);
+    text("Press right and left arrow to make jack go right and left",50,80);
   }
 
 
@@ -61,7 +58,7 @@ function draw() {
   if (gameState==PLAY){
     if(keyDown("up"))
     {
-    player.velocityY=-10;
+      player.velocityY=-10;
     }
     if(keyDown("left"))
     {
@@ -79,9 +76,9 @@ function draw() {
     player.collide(platformGroups);
 
   
-  spawnPlatform();
+    spawnPlatform();
 
-}
+  }
 
   drawSprites();
   
@@ -93,29 +90,29 @@ function spawnPlatform(){
     platform.destroy();
    
     var platforms = createSprite(250,0,50,50);
-  platforms.velocityY = 5;
+    platforms.velocityY = 5;
 
-  var r = Math.round(random(1,6))
-  switch (r){
-    case 1 : platforms.addImage(platform1);
-    break;
-    case 2 : platforms.addImage(platform2);
-    break;
-    case 3 : platforms.addImage(platform3);
-    break;
-    case 4 : platforms.addImage(platform4);
-    break;
-    case 5 : platforms.addImage(platform5);
-    break;
-    case 6 : platforms.addImage(platform6);
-    break;
-  }
- 
-  platforms.scale = 0.1;
+    var r = Math.round(random(1,6))
+    switch (r){
+      case 1 : platforms.addImage(platform1);
+      break;
+      case 2 : platforms.addImage(platform2);
+      break;
+      case 3 : platforms.addImage(platform3);
+      break;
+      case 4 : platforms.addImage(platform4);
+      break;
+      case 5 : platforms.addImage(platform5);
+      break;
+      case 6 : platforms.addImage(platform6);
+      break;
+    }
 
-  platformGroups.add(platforms);
+    platforms.scale = 0.1;
 
-  platforms.x = random(0,500);
-  platforms.lifetime = 200;
+    platformGroups.add(platforms);
+
+    platforms.x = random(0,500);
+    platforms.lifetime = 200;
   }
 }
